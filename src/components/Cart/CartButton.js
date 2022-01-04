25da14aa77f70c8a6c94a9fb06c8717f9fd40c5c
Manjado <michalalchimowicz@gmail.com>
2022-01-04 20:23:11 +0100

    added redux-toolkit

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,21 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux"
+
+import { uiActions } from "../../store/ui-slice"
+import classes from "./CartButton.module.css"
 
 const CartButton = (props) => {
+  const dispatch = useDispatch()
+
+  const toggleCrtHandler = () => {
+    dispatch(uiActions.toggle())
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCrtHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
-  );
-};
+  )
+}
 
-export default CartButton;
+export default CartButton
