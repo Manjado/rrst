d84af78577c572f7c3b2dbf86e116b7fce47046d
Manjado <michalalchimowicz@gmail.com>
2022-01-05 22:44:36 +0100

    dummy product

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,37 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem"
+import classes from "./Products.module.css"
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "JS book",
+    description: "You do not know JS",
+  },
+  {
+    id: "p2",
+    price: 8,
+    title: "TS book",
+    description: "You do not know TS",
+  },
+]
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(({ title, price, description, key }) => (
+          <ProductItem
+            key={key}
+            title={title}
+            price={price}
+            description={description}
+          />
+        ))}
       </ul>
     </section>
-  );
-};
+  )
+}
 
-export default Products;
+export default Products
